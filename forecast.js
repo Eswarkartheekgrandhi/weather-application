2375e582591f66e2fcc9bb32e0c5567a97a13f3b
Eswar Kartheek Grandhi <85437753+Eswarkartheekgrandhi@users.noreply.github.com>
2022-10-31 22:55:43 +0530

    Add files via upload

---
+++ forecast.js
@@ -0,0 +1,23 @@
+const key = "1Ajtj1XKND31WML32GPOpZIp7Gs8ur9j";
+
+//get weather information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+//get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
